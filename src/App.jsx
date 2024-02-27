fb05320f64afbfcdb17db5836ded00037dd4c2e3
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2024-02-27 15:05:20 +0530

    made basic overview of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+import Post from './components/Post';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className='app-container'>
+        <Sidebar/>
+        <div className='content'>
+          <Header/>
+          <CreatePost/>
+          <Post/>
+          <Footer/>
+        </div>
+      </div>
     </>
   )
 }
