98ebaaca48cd1d787c96be599962837df52580e7
Deepanshi1523 <104934054+Deepanshi1523@users.noreply.github.com>
2024-03-09 05:36:11 +0530

    added delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
 import CreatePost from './components/CreatePost';
-import Post from './components/Post';
+import PostList from './components/PostList';
+import { useState } from 'react';
+import PostListProvider from './store/post-list-store';
+import './App.css';
 
 function App() {
+  const [selectedTab, setSelectedTab]=useState("Home")
+
   return (
-    <>
+    <PostListProvider>
       <div className='app-container'>
-        <Sidebar/>
+        <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}/>
         <div className='content'>
           <Header/>
-          <CreatePost/>
-          <Post/>
+          {selectedTab==="Home"?(
+            <PostList/>
+          ):(
+            <CreatePost/>
+          )}
           <Footer/>
         </div>
       </div>
-    </>
+    </PostListProvider>
   )
 }
 
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,32 @@
 const CreatePost = () => {
   return (
-    <form>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <form className="create-post">
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">Enter your User Id here</label>
+        <input type="text" className="form-control" id="userId" placeholder="Your User Id"/>
       </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1"/>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">Post Title</label>
+        <input type="text" className="form-control" id="title" placeholder="How are you feeling today..."/>
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">Post Content</label>
+        <textarea type="text" rows={4} className="form-control" id="body" placeholder="Tell us more about it"/>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">Number of reactions</label>
+        <input type="text" className="form-control" id="reactions" placeholder="Hom many people reacted to this post"/>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">Enter your hashtags here</label>
+        <input type="text" className="form-control" id="tags" placeholder="Please enter tags using space"/>
+      </div>
+      
+      <button type="submit" className="btn btn-primary">Post</button>
     </form>
   )
 }
